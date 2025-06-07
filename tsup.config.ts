8fb095412536e2e1392ca71ad8fcf54ebf294af9
gkzhb <gkzhb98@gmail.com>
2025-06-08 02:50:48 +0800

    chore: update tsup config to output specified js file so that npx can recognize js bin file

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,17 +1,20 @@
 import type { Options } from 'tsup'
 import { defineConfig } from 'tsup'
 
+const commonOptions: Options = {
+  entry: { 'crawl4ai-mcp': 'src/index.ts' },
+}
 export default defineConfig((options) => {
   const buildOptions: Options = {
+    ...commonOptions,
     name: 'Build',
-    entry: ['src/index.ts'],
     splitting: false,
     minify: true,
   }
   const devOptions: Options = {
+    ...commonOptions,
     name: 'Develop',
-    entry: ['src/index.ts'],
-    onSuccess: 'node dist/index.js',
+    onSuccess: 'node dist/crawl4ai-mcp.js',
   }
   return options.watch ? devOptions : buildOptions
 })
